86730b889d450b0e57e467370ecb8032f6be276b
Magdy Ali <125136052+Migo090@users.noreply.github.com>
2023-02-23 19:43:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+link = `<p class="link">
+Challenge by 
+<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
+.
+Coded by 
+<a href="https://www.frontendmentor.io/profile/Migo090">Magdy Ali</a>
+</p>`
+document.querySelector("body").innerHTML += link;
+function display(){
+    document.getElementById("rating-first").style.display = "none";
+    document.getElementById("rating-second").style.display = "flex";
+}
+function rate(){
+    for(let i =1;i<6;i++){
+        choose = document.getElementById(i);
+        if (choose.checked){
+            document.getElementById("result").innerText = `You selected ${i} out of 5`;
+        }
+    }
+}
