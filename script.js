a328e2f7c401e8cb24e00f1a5ef7d731065a0a57
Magdy Ali <125136052+Migo090@users.noreply.github.com>
2023-02-23 20:17:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-link = `<p class="link">
+link = `<footer><p class="link">
 Challenge by 
 <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
 .
 Coded by 
 <a href="https://www.frontendmentor.io/profile/Migo090">Magdy Ali</a>
-</p>`
+</p></footer>`
 document.querySelector("body").innerHTML += link;
 function display(){
     document.getElementById("rating-first").style.display = "none";
